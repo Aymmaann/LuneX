654202b46877417a73008557bf25ca62c9cd6b7e
Ayman Haseeb <aymanhaseeb8121@gmail.com>
2025-04-03 19:23:21 +0530

    fix: resolve Google OAuth2 client issue

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -8,10 +8,6 @@ const googleLogin = async (req, res) => {
     const { code } = req.query;
     const googleRes = await oauth2client.getToken(code);
     oauth2client.setCredentials(googleRes.tokens);
-    
-    // const userRes = await axios.get(
-    //   `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${googleRes.tokens.access_token}`
-    // );
     const userRes = await axios.get(
       `https://www.googleapis.com/oauth2/v1/userinfo?alt=json`,
       {
