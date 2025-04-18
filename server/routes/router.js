8634d05a61cce4778c9e34c4a1c1658dcb812b46
Ayman Haseeb <aymanhaseeb8121@gmail.com>
2025-04-18 20:28:35 +0530

    feat: Add new endpoint for serving crypto analysis data

--- server/routes/router.js
+++ server/routes/router.js
@@ -7,6 +7,9 @@ import {
   triggerCryptoUpdate,
   updateAllUsersCoins
 } from "../controllers/coinController.js";
+import { exportAllQueries } from '../utils/exportAnalysis.js';
+import { authMiddleware } from '../middleware/authMiddleware.js';
+import { getCryptoBigQueryAnalysis } from '../handlers/cryptoAnalysisHandler.js';
 
 const router = express.Router();
 
@@ -20,5 +23,17 @@ router.get('/saved-coins', getUserCoins);
 router.post('/update-crypto-values', updateCryptoValues);
 router.get('/api/trigger-crypto-update', triggerCryptoUpdate);
 router.get('/api/update-all-users', updateAllUsersCoins);
+
+router.get('/api/crypto-analysis', authMiddleware, getCryptoBigQueryAnalysis);
+
+router.post('/api/export/crypto-analytics', async(req,res) => {
+  try {
+    await exportAllQueries()
+    res.status(200).json({ message: 'Export Complete' })
+  } catch(error) {
+    console.error('Export failed:', error.message);
+    res.status(500).json({ error: 'Export failed' });
+  }
+})
 
 export default router;
